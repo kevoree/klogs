b09a769980d06daba597d773623d0b9affca3633
Maxime Tricoire <max.tricoire@gmail.com>
2017-02-28 17:29:19 +0100

    v1.0.1: fix ^C preventing to output stopping logs

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
 #! /usr/bin/env node
+
 var readline = require('readline');
 var chalk = require('chalk');
 
 var REGEX = /^([0-9]{2}:[0-9]{2}) (TRACE|DEBUG|INFO|WARN|ERROR): (.*)$/;
 
 function noColor(str) {
-  return str;
+	return str;
 }
 
 function prettyPrint(match, time, level, msg) {
-  var color;
+	var color;
 
-  switch (level) {
-    default:
-      color = noColor;
-      break;
+	switch (level) {
+		default: color = noColor;
+		break;
 
-    case 'TRACE':
-      color = chalk.grey;
-      break;
+		case 'TRACE':
+				color = chalk.grey;
+			break;
 
-    case 'DEBUG':
-      color = chalk.cyan;
-      break;
+		case 'DEBUG':
+				color = chalk.cyan;
+			break;
 
-    case 'INFO':
-      color = noColor;
-      break;
+		case 'INFO':
+				color = noColor;
+			break;
 
-    case 'WARN':
-      color = chalk.yellow;
-      break;
+		case 'WARN':
+				color = chalk.yellow;
+			break;
 
-    case 'ERROR':
-      color = chalk.red;
-      break;
-  }
-  return chalk.gray(time) + '  ' + color(msg);
+		case 'ERROR':
+				color = chalk.red;
+			break;
+	}
+	return chalk.gray(time) + '  ' + color(msg);
 }
 
-var rl = readline.createInterface({ input: process.stdin });
+var rl = readline.createInterface({
+	input: process.stdin
+});
 
 rl.on('line', function (line) {
-  console.log(line.replace(REGEX, prettyPrint));
+	console.log(line.replace(REGEX, prettyPrint));
+});
+
+process.on('SIGINT', function () {
+	// process.stdout.write('\x1b[0G');
 });
