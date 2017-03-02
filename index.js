18772360a46825a299243e5f9eb09b5a7cfedbe7
Maxime Tricoire <max.tricoire@gmail.com>
2017-03-02 15:24:38 +0100

    v1.0.2: better colors

--- index.js
+++ index.js
@@ -3,48 +3,48 @@
 var readline = require('readline');
 var chalk = require('chalk');
 
-var REGEX = /^([0-9]{2}:[0-9]{2}) (TRACE|DEBUG|INFO|WARN|ERROR): (.*)$/;
+var REGEX = /^([0-9]{2}:[0-9]{2}) (TRACE|DEBUG|INFO|WARN|ERROR): (.*)/;
 
 function noColor(str) {
 	return str;
 }
 
-function prettyPrint(match, time, level, msg) {
-	var color;
-
+function getColor(level) {
 	switch (level) {
-		default: color = noColor;
-		break;
+		default:
+			return noColor;
 
 		case 'TRACE':
-				color = chalk.grey;
-			break;
+			return chalk.grey;
 
 		case 'DEBUG':
-				color = chalk.cyan;
-			break;
+			return chalk.cyan;
 
 		case 'INFO':
-				color = noColor;
-			break;
+			return noColor;
 
 		case 'WARN':
-				color = chalk.yellow;
-			break;
+			return chalk.yellow;
 
 		case 'ERROR':
-				color = chalk.red;
-			break;
+			return chalk.red;
 	}
-	return chalk.gray(time) + '  ' + color(msg);
 }
 
 var rl = readline.createInterface({
 	input: process.stdin
 });
 
+var matcher;
+var color = noColor;
 rl.on('line', function (line) {
-	console.log(line.replace(REGEX, prettyPrint));
+	matcher = line.match(REGEX);
+	if (matcher) {
+		color = getColor(matcher[2]);
+		console.log(chalk.gray(matcher[1]) + '  ' + color(matcher[3]));
+	} else {
+		console.log(color(line));
+	}
 });
 
 process.on('SIGINT', function () {
